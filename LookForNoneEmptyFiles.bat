4ac2269f756ba84a3424d22afd96b214c6384fa5
sjuma-sys <93128573+sjuma-sys@users.noreply.github.com>
2025-01-14 07:11:23 +0000

    Update LookForNoneEmptyFiles.bat

--- LookForNoneEmptyFiles.bat
+++ LookForNoneEmptyFiles.bat
@@ -1,2 +1,2 @@
-:: A way to find none empty files, forfiles is more powerful as it can do it in a time range
-forfiles /P J:\MichealCasey\V10 /M *.* /S /D +"01/01/1970" /C "cmd /c if @fsize gtr 0 echo @path @fsize @fdate @ftime" | findstr /r "\.emlx \.pdf \.docx \.xlsx \.doc"
+:: A way to find none empty files within files, forfiles is more powerful as it can do it in a time range, also you can change the findstr to look for other file extensions
+forfiles /P J:\Foldername /M *.* /S /D +"01/01/1970" /C "cmd /c if @fsize gtr 0 echo @path @fsize @fdate @ftime" | findstr /r "\.emlx \.pdf \.docx \.xlsx \.doc"
